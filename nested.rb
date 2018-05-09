cad6386228950fd87d9ddc312ff334fe49e7c1cf
Michael Ballard <ballard.michael9@gmail.com>
2018-05-09 13:10:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  
+  programmer_hash[:yukihiro_matsumoto]
     
 end
 
